2718520de67d4a78c25de05e3f969dcf36bcdd4b
isha2727 <ishagrg2727@gmail.com>
2025-02-23 21:39:40 +0545

    added converter

--- quizapp/question_model.py
+++ quizapp/question_model.py
@@ -1,3 +1,4 @@
+# Create a question class with an __init()__ method with two attributes:text and answer attribute.
 class Question:
     def __init__(self, q_text, q_answer):
         self.text= q_text
